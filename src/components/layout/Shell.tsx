ef9d8a55e2a3ef7ff72984d7b991fb175c0e9146
Romain Lanz <romain.lanz+github@pm.me>
2022-09-01 22:05:58 +0200

    feat: add toasting

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -1,4 +1,5 @@
-import { styled } from '~/stitches.config';
+import { ToastBar, Toaster } from 'react-hot-toast';
+import { keyframes, styled } from '~/stitches.config';
 import { Header } from '~/components/layout/Header';
 import { Sidebar } from '~/components/layout/Sidebar';
 import { Grid } from '~/components/ui';
@@ -34,6 +35,16 @@ const ShellWrapper = styled(Grid, {
 	},
 });
 
+const slideIn = keyframes({
+	'0%': { transform: 'translateX(100%)', opacity: 0 },
+	'100%': { transform: 'translateX()', opacity: 1 },
+});
+
+const slideOut = keyframes({
+	'0%': { transform: 'translateX(0%)', opacity: 1 },
+	'100%': { transform: 'translateX(100%)', opacity: 0 },
+});
+
 export function Shell({ children }: { children: ReactNode }) {
 	return (
 		<ShellWrapper>
@@ -43,6 +54,34 @@ export function Shell({ children }: { children: ReactNode }) {
 				<Header />
 				<main>{children}</main>
 			</MaxWidthWrapper>
+
+			<Toaster
+				toastOptions={{
+					className: '',
+					style: {
+						backgroundColor: 'hsl(168,26%,93%)',
+						borderRadius: '5px',
+						boxShadow: '$base',
+						padding: '8px 16px',
+					},
+				}}
+				containerStyle={{
+					right: 50,
+					top: 100,
+				}}
+				position="top-right"
+				reverseOrder={false}
+			>
+				{(t) => (
+					<ToastBar
+						toast={t}
+						style={{
+							...t.style,
+							animation: t.visible ? `${slideIn} 300ms ease-out forwards` : `${slideOut} 200ms ease-in forwards`,
+						}}
+					/>
+				)}
+			</Toaster>
 		</ShellWrapper>
 	);
 }
