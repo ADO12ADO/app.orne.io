4240e9dd2f1510f32c37ce396277b2e26bef4598
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-01-12 21:14:36 +0800

    Update WithdrawForm.tsx

--- apps/orne-dapp/src/components/earn/WithdrawForm.tsx
+++ apps/orne-dapp/src/components/earn/WithdrawForm.tsx
@@ -13,7 +13,7 @@ import { readAmount } from '~/utils/readAmount';
 import { Button } from '../ui/Button';
 import { IconToken } from '../ui/IconToken';
 
-export function WithdrawForm() {
+const WithdrawForm = () => {
 	const { data: lpBalance, isLoading } = useLPBalance();
 
 	const [amount, setAmount] = useState('');
@@ -108,4 +108,5 @@ export function WithdrawForm() {
 			</Button>
 		</>
 	);
-}
+};
+export default WithdrawForm;
