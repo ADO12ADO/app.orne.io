5f7766743d402c3de1b6fafc5933c8a3cdba08f5
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-01-12 19:35:05 +0800

    Update ProvideForm.tsx

--- apps/orne-dapp/src/components/earn/ProvideForm.tsx
+++ apps/orne-dapp/src/components/earn/ProvideForm.tsx
@@ -14,7 +14,7 @@ import { readAmount } from '~/utils/readAmount';
 import { Button } from '../ui/Button';
 import { IconToken } from '../ui/IconToken';
 
-export function ProvideForm() {
+export function ProvideFormV2() {
 	const { data: orneBalance, isLoading: isLoadingOrneBalance } = useOrneBalance();
 	const { data: lunaBalance, isLoading: isLoadingLunaBalance } = useLunaBalance();
 
