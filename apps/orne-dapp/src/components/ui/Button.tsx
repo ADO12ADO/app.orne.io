02c2cdfd9f97c63fbcf1d0c207ec2964580335e8
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-01-12 22:44:08 +0800

    Update Button.tsx

--- apps/orne-dapp/src/components/ui/Button.tsx
+++ apps/orne-dapp/src/components/ui/Button.tsx
@@ -3,36 +3,34 @@ import { forwardRef } from 'react';
 import type { ReactNode } from 'react';
 
 type ButtonProps = {
-  children: ReactNode;
-  onClick?: () => void;
-  className?: string;
-  variant?: 'fill' | 'outline';
-  size?: 'sm' | 'lg';
-  type?: 'button' | 'submit' | 'reset';
+	children: ReactNode;
+	onClick?: () => void;
+	className?: string;
+	variant?: 'fill' | 'outline';
+	size?: 'sm' | 'lg';
+	type?: 'button' | 'submit' | 'reset';
 };
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
-  { children, variant = 'fill', className, onClick, size = 'lg', type = 'button' },
-  ref
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+	{ children, variant = 'fill', className, onClick, size = 'lg', type = 'button' },
+	ref
 ) {
-  const classes = clsx(
-    'inline-flex items-center justify-center font-semibold px-4 py-2 border border-transparent rounded-full transition-colors',
-    {
-      'bg-green text-darkGreen hover:bg-green75': variant === 'fill',
-      'text-green border-2 !border-green hover:bg-green hover:text-darkGreen': variant === 'outline',
-    },
-    {
-      'h-8 px-3 text-sm': size === 'sm',
-      'h-12 w-full text-base': size === 'lg',
-    },
-    className
-  );
+	const classes = clsx(
+		'inline-flex items-center justify-center font-semibold px-4 py-2 border border-transparent rounded-full transition-colors',
+		{
+			'bg-green text-darkGreen hover:bg-green75': variant === 'fill',
+			'text-green border-2 !border-green hover:bg-green hover:text-darkGreen': variant === 'outline',
+		},
+		{
+			'h-8 px-3 text-sm': size === 'sm',
+			'h-12 w-full text-base': size === 'lg',
+		},
+		className
+	);
 
-  return (
-    <button className={classes} ref={ref} type={type} onClick={onClick}>
-      {children}
-    </button>
-  );
+	return (
+		<button className={classes} ref={ref} type={type} onClick={onClick}>
+			{children}
+		</button>
+	);
 });
-
-export default Button; // Ensure this line exports Button as the default export
